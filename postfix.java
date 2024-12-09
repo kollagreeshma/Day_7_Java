40fed5a8149dbcf026d8bf85c434866aba943c57
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-09 10:45:28 +0530

    Add files via upload

---
+++ postfix.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class postfix{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=n;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        int post[]=new int[m];
+        post[0]=arr[(n-1)];
+        m--;
+        for(int i=1;i<n;i++){
+            m--;
+            post[i]=post[(i-1)]
+            +arr[m];
+        }
+        System.out.println(Arrays.toString(post));
+        }
+    }
+
